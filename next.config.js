e51a2ebaccad4401501c04a1ac923366dc9aff2c
felipe <fepinzon77@gmail.com>
2023-04-19 10:34:18 -0500

    ConfigC

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: { images: { layoutRaw: true } },
+  images: {
+    domains: ['images.unsplash.com', 'images.pexels.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
