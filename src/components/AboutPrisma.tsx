ea1dd60f092d6c821665af45e774e6c0545afcb4
felipe <fepinzon77@gmail.com>
2023-04-25 21:43:43 -0500

    [FIX] responsive AboutPrisma

--- src/components/AboutPrisma.tsx
+++ src/components/AboutPrisma.tsx
@@ -1,4 +1,5 @@
 import { MapPinIcon, PresentationChartLineIcon, PuzzlePieceIcon, UserGroupIcon } from '@heroicons/react/24/solid';
+import { Container } from './Container';
 
 const features = [
   {
@@ -26,29 +27,31 @@ const features = [
 export function AboutPrisma() {
   return (
     <section id="AboutPrisma" className="py-20 sm:py-32">
-      <div className="mx-auto max-w-2xl sm:text-center">
-        <h2 className="text-3xl font-medium tracking-tight text-orange-500">Características principales.</h2>
-        <p className="mt-2 text-lg text-gray-600">Con nuestra aplicación de gestión de personal, podrás gestionar de manera fácil y eficiente el personal en misión.</p>
-      </div>
-      <ul className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 text-sm sm:mt-20 sm:grid-cols-2 md:gap-y-10 lg:max-w-none lg:grid-cols-2" style={{ padding: '0 8rem' }}>
-        {features.map((feature) => (
-          <li key={feature.name} className="rounded-2xl border border-gray-200 p-8">
-            <div className="flex items-center">
-              <feature.icon className="h-8 w-8 text-orange-500" />
-              <h3 className="pl-2 font-semibold text-gray-900">{feature.name}</h3>
-            </div>
-            <p className="mt-2 text-gray-700">{feature.description}</p>
-          </li>
-        ))}
-      </ul>
-      <div className="mt-10 flex justify-center">
-        <a
-          href="https://www.mainprisma.com/"
-          className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
-        >
-          Explora nuestra Aplicacion
-        </a>
-      </div>
+      <Container>
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-3xl font-medium tracking-tight text-orange-500">Características principales.</h2>
+          <p className="mt-2 text-lg text-gray-600">Con nuestra aplicación de gestión de personal, podrás gestionar de manera fácil y eficiente el personal en misión.</p>
+        </div>
+        <ul className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 text-sm sm:mt-20 sm:grid-cols-2 md:gap-y-10 lg:max-w-none lg:grid-cols-2">
+          {features.map((feature) => (
+            <li key={feature.name} className="rounded-2xl border border-gray-200 p-8">
+              <div className="flex items-center">
+                <feature.icon className="h-8 w-8 text-orange-500" />
+                <h3 className="pl-2 font-semibold text-gray-900">{feature.name}</h3>
+              </div>
+              <p className="mt-2 text-gray-700">{feature.description}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="mt-10 flex justify-center">
+          <a
+            href="https://www.mainprisma.com/"
+            className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
+          >
+            Explora nuestra Aplicacion
+          </a>
+        </div>
+      </Container>
     </section>
   );
 }
